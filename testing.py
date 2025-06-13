967de11cbfeaed4466156ebb8137d2d6d9d16c63
root <root@ip-172-31-81-146.ec2.internal>
2025-06-13 11:02:30 +0000

    hello

--- testing.py
+++ testing.py
@@ -1,1 +1,8 @@
-echo 'print("hello Dosto")' > testing.py
+"""This is a sample test script that prints a greeting message."""
+
+def main():
+    print("hello Dosto")
+
+if __name__ == "__main__":
+    main()
+
